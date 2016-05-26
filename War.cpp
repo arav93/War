3c7ddc2bbc4f778675b661a19d4410cc93780acc
Aravind K N <aravikn@gmail.com>
2016-05-26 18:48:26 +0530

    Create War.cpp

---
+++ War.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<vector>
+#include <stdlib.h> /*srand,rand */
+#include <time.h>  /*time */
+
+using namespace std;
+
+vector<int> A;
+vector<int> B;
+
+void init_army(vector<float>& ArmyA,int size)
+{
+    int army_1 = rand()%(size-2)+1;  //Minimum one person present in each position
+    ArmyA.push_back(army_1);
+    int army_2 = rand()%(size-army_1-1)+1;
+    ArmyA.push_back(army_2);
+    int army_3 = size - army_1-army_2;
+    ArmyA.push_back(army_3);
+
+}
+
+void copy(vector<int> P, vector<int> Q)
+{
+    for(int i=0;i<3;i++)
+    {
+        A.push_back(P[i]);
+        B.push_back(Q[i]);
+    }
+}
+
+void display(vector<float> ArmyA, vector<float> ArmyB)
+{
+    cout<<"A:"<<endl;
+    for(int i=0;i<3;i++)
+    {
+        cout<<((ArmyA[i])/A[i])*100<<" only of type "<<i<<endl;
+    }
+    cout<<"B:"<<endl;
+    for(int i=0;i<3;i++)
+    {
+        cout<<((ArmyB[i])/B[i])*100<<" only of type "<<i<<endl;
+    }
+}
+
+void start(vector<float> &A , vector<float> &B)
+{
+    B[0]=B[0]-0.5*A[0]-0.2*A[1]-0.1*A[2];
+    B[1]=B[1]-0.8*A[0]-0.5*A[1]-0.2*A[2];
+    B[2]=B[2]-0.8*A[0]-0.2*A[1]-0.5*A[2];
+
+    if(B[0]<0)
+        B[0]=0;
+    if(B[1]<0)
+        B[1]=0;
+    if(B[2]<0)
+        B[2]=0;
+
+}
+
+bool isDead(vector<float> &A)
+{
+    return (A[0]<=0 && A[1]<=0 && A[2]<=0);
+}
+
+void fight(vector<float> &A, vector<float> &B)
+{
+
+    while(!isDead(A)&&!isDead(B))
+    {
+        start(A,B);
+        display(A,B);
+        start(B,A);
+        display(A,B);
+        count++;
+    }
+
+    if(isDead(A))
+        cout<<"B won the fight!";
+    else
+        cout<<"A won the fight!";
+
+
+}
+
+int main()
+{
+
+    int size;
+    srand((int)time(0));
+    vector<float> ArmyA;  //A and B are the different armies
+    vector<float> ArmyB;
+    copy(ArmyA,ArmyB)
+    cout<<"Enter the size of the army:\n->";
+    cin>>size;
+    init_army(ArmyA,size);
+    init_army(ArmyB,size);
+    fight(ArmyA,ArmyB);
+    return 0;
+}
